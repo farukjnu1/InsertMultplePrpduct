4d9601c41858103825da8602435eef59c538f922
farukjnu1@gmail.com <farukjnu1@gmail.com>
2025-08-14 04:41:38 +0800

    Add project files.

---
+++ InsertMultplePrpduct/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using InsertMultplePrpduct.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InsertMultplePrpduct.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private Database1Entities db = new Database1Entities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            //var db = new Model1();
+            var listCategory = db.Categories.ToList();
+            return View(listCategory);
+        }
+        [HttpPost]
+        public ActionResult AddProduct(List<Product> ListProduct)
+        {
+            //var db = new Model1();
+            db.Products.AddRange(ListProduct);
+            db.SaveChangesAsync();
+            var result = new { message = ListProduct.Count + " product(s) save successfully." };
+            return Json(result);
+        }
+    }
+}
---
+++ InsertMultplePrpduct/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InsertMultplePrpduct.MvcApplication" Language="C#" %>
